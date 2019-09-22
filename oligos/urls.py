bd32372e5bd98171ab60c2d64a7b12b13c7293ed
Abeer Eltanawy <abeer.eltanawy@schrodinger.com>
2019-09-22 23:54:25 +0200

    Created views and templates for oligos app

--- oligos/urls.py
+++ oligos/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 
 from . import views
 
+app_name = 'oligos'
 urlpatterns = [
+    # /oligos/
     path('', views.index, name='index'),
+    # /oligos/<int:oligo_id>/
+    path('<int:oligo_id>/', views.detail, name='detail'),
+    # /oligos/<int:oligo_id>/edit/
+    path('<int:oligo_id>/edit/', views.edit, name='edit'),
+    # /oligos/<int:oligo_id>/delete/
+    path('<int:oligo_id>/delete/', views.delete, name='delete'),
+    # /oligos/batch/
+    path('batch/', views.batch, name='batch'),
+    # /oligos/search/
+    path('search/', views.search, name='search'),
 ]
--- oligos/views.py
+++ oligos/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+#from django.template import loader
+from .models import Oligo
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You'r are in the oligos index.")
+    oligo_set = Oligo.objects.all()
+    #template = loader.get_template('oligos/index.html')
+    context = {
+        'oligo_set' : oligo_set,
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'oligos/index.html', context)
+
+def detail(request, oligo_id):
+    oligo = get_object_or_404(Oligo, pk=oligo_id)
+    return render(request, 'oligos/detail.html', {'oligo': oligo})
+
+def edit(request, oligo_id):
+    return HttpResponse(f"Editing {oligo_id}.")
+
+def delete(request, oligo_id):
+    return HttpResponse(f"Deleting {oligo_id}.")
+
+def batch(request):
+    return HttpResponse("Batch Oligo")
+
+def search(request):
+    return HttpResponse("Search Oligo")
