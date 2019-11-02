98e17397dd2213107414a253888ef51a2fc91b0e
Abeer Eltanawy <abeer.eltanawy@gmail.com>
2019-11-02 23:37:01 +0100

    Added change password feature using Django auth app

--- receptor/settings.py
+++ receptor/settings.py
@@ -134,3 +134,6 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
