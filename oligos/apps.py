5f67ff6ecefcc05655adda8956baa00f5e7cbae7
Abeer Eltanawy <abeer.eltanawy@schrodinger.com>
2021-05-27 00:16:41 +0200

    Commented out pagination handling from the backend

--- oligos/apps.py
+++ oligos/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+# from rest_framework.pagination import PageNumberPagination
+# from rest_framework.response import Response
 
 
 class OligosConfig(AppConfig):
     name = 'oligos'
+
+
+# class CustomPagination(PageNumberPagination):
+#     def get_paginated_response(self, data):
+#         return Response({
+#             'links': {
+#                 'next': self.get_next_link(),
+#                 'previous': self.get_previous_link()
+#             },
+#             'count': self.page.paginator.count,
+#             'results': data
+#         })
